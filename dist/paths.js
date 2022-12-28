f7066873586cfa38efb2b2cc6e449f049a699ea9
reynaldo-dev <reynaldo23.dev@gmail.com>
2022-12-28 08:53:53 -0600

    [ADD] helper for validation schema

--- dist/paths.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Path = void 0;
-exports.Path = {
-    Root: '/',
-    Users: '/users',
-};
-//# sourceMappingURL=paths.js.map
--- dist/user/user.routes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const user_controller_1 = require("./user.controller");
-const router = (0, express_1.Router)();
-router.post('/', user_controller_1.postUser);
-exports.default = router;
-//# sourceMappingURL=user.routes.js.map
